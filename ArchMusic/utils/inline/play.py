b1c059e0d3679be95d6d600ff09c631e2acf64fa
SmartGirl15 <92010030+SmartGirl15@users.noreply.github.com>
2024-07-29 23:14:43 +0530

    Update play.py

--- ArchMusic/utils/inline/play.py
+++ ArchMusic/utils/inline/play.py
@@ -166,19 +166,19 @@ def stream_markup(_, videoid, chat_id, played, dur):
     return buttons
 
 
-def telegram_markup(_, chat_id):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text=_["PL_B_3"],
-                callback_data=f"PanelMarkup None|{chat_id}",
-            ),
-            InlineKeyboardButton(
-                text=_["CLOSEMENU_BUTTON"], callback_data="close"
-            ),
-        ],
-    ]
-    return buttons
+#def telegram_markup(_, chat_id):
+    #buttons = [
+        #[
+            #InlineKeyboardButton(
+                #text=_["PL_B_3"],
+                #callback_data=f"PanelMarkup None|{chat_id}",
+            #),
+            #InlineKeyboardButton(
+                #text=_["CLOSEMENU_BUTTON"], callback_data="close"
+            #),
+        #],
+    #]
+    #return buttons
 
 
 ## Search Query Inline
